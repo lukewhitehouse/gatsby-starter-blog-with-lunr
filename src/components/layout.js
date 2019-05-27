24605e724a581ec8f4aa39d634fd3e197ecb4029
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2019-05-27 21:32:38 +0100

    Add styling for site search components

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import { rhythm, scale } from "../utils/typography"
+import "../styles/with-lunr.css"
 
 class Layout extends React.Component {
   render() {
