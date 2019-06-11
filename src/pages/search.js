3d67e61a70381d6e283aea4e50b76e1f754c59d5
Mike Francis <mikeffrancis@gmail.com>
2019-06-11 08:55:01 +0100

    Use lodash.debounce instead of lodash

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import debounce from "lodash/debounce"
+import debounce from "lodash.debounce"
 import Layout from "../components/layout"
 import SearchForm from "../components/searchForm"
 import SearchResults from "../components/searchResults"
